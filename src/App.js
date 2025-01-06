153db4e420c82b9e857b5901f378ea70de74b97e
Elliot Bartel <elliotbartel@elliots-air.lan>
2025-01-06 17:44:09 -0500

    Use props to pass colors

--- src/App.js
+++ src/App.js
@@ -3,13 +3,51 @@ import './App.css';
 import ColorPalette from './ColorPallete';
 
 function App() {
+  const colors1 = {
+    complementaryColors: [
+      { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' },
+      { code: '17-3934', name: 'Persian Jewel', color: '#7B88C7' },
+      { code: '19-3952', name: 'Surf the Web', color: '#1B365D' }
+    ],
+    neutrals: [
+      { code: '19-3933', name: 'Medieval Blue', color: '#253046' },
+      { code: '11-4001', name: 'Brilliant White', color: '#F5F5FF' }
+    ],
+    accents: [
+      { code: '11-0620', name: 'Elfin Yellow', color: '#F3E5AB' },
+      { code: '13-6008', name: 'Misty Jade', color: '#B5D5C5' },
+      { code: '16-3617', name: 'Sheer Lilac', color: '#E0B0FF' },
+      { code: '19-3730', name: 'Gentian Violet', color: '#483248' }
+    ],
+  }
+  
+  const colors2 = {
+      complementaryColors: [
+      { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' },
+      { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' },
+      { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' }
+    ],
+    neutrals: [
+      { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' },
+      { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' }
+    ],
+    accents: [
+      { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' },
+      { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' },
+      { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' },
+      { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' }
+    ],
+  }
+
   return (
     <>
-      <div className="flex flex-wrap">
-        <ColorPalette />
-        <ColorPalette />
-        <ColorPalette />
-        <ColorPalette />
+      <div className="flex flex-wrap gap-4">
+        <ColorPalette 
+          colors={colors1}
+        />
+        <ColorPalette 
+          colors={colors2}
+        />
       </div>
     </>
   );
--- src/ColorPallete.js
+++ src/ColorPallete.js
@@ -1,29 +1,9 @@
 import React from 'react';
 
-const ColorPalette = () => {
-  const complementaryColors = [
-    { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' },
-    { code: '17-3934', name: 'Persian Jewel', color: '#7B88C7' },
-    { code: '19-3952', name: 'Surf the Web', color: '#1B365D' }
-  ];
-
-  const neutrals = [
-    { code: '19-3933', name: 'Medieval Blue', color: '#253046' },
-    { code: '11-4001', name: 'Brilliant White', color: '#F5F5FF' }
-  ];
-// grid-cols-[repeat(7,_1fr)_auto]
-// grid-cols-
-// grid-template-columns: 1fr 1fr 1fr 1fr ... auto
-  const accents = [
-    { code: '11-0620', name: 'Elfin Yellow', color: '#F3E5AB' },
-    { code: '13-6008', name: 'Misty Jade', color: '#B5D5C5' },
-    { code: '16-3617', name: 'Sheer Lilac', color: '#E0B0FF' },
-    { code: '19-3730', name: 'Gentian Violet', color: '#483248' }
-  ];
-
+const ColorPalette = ({colors: {complementaryColors, neutrals, accents}}) => {
   return (
     <>
-    <div className="grid grid-cols-7 grid-rows-4 gap-4 h-72 w-115">
+    <div className="grid grid-cols-[repeat(7,_1fr)_auto] grid-rows-4 gap-4 h-72 w-115">
 
     {/* React needs "key" to identify each div relative to parent */}
     {complementaryColors.map((color, i) => (
@@ -31,13 +11,14 @@ const ColorPalette = () => {
         <div 
         key={color.name}
         className={`relative col-span-2 row-span-3 col-start-${i * 2 + 1}`} 
-        style={{ backgroundColor: color.color }}>
+        style={{ backgroundColor: color.color }}
+        // handleClick={onColorClick(color.name)}
+        >
             <div className="absolute bottom-0 m-2">
                 <div>{color.name}</div>
                 <div>{color.code} TCX</div>
             </div>
         </div>
-
         
     ))}
     {neutrals.map((color, i) => (
@@ -52,15 +33,17 @@ const ColorPalette = () => {
         </div>
     ))}
     {accents.map((color, i) => (
+      <>
         <div 
         key={color.name}
-        className={`relative col-start-7 row-start-${i}`} 
+        className={`col-start-7 row-start-${i}`} 
         style={{ backgroundColor: color.color }}>
-            <div className="absolute bottom-0 -right-28 m-2">
-                <div>{color.name}</div>
-                <div>{color.code} TCX</div>
-            </div>
         </div>
+        <div className={`col-start-8 row-start-${i}`}>
+          <div>{color.name}</div>
+          <div>{color.code} TCX</div>
+        </div>
+      </>
     ))}
     </div>
     </>
