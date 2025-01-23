fd7d51757b241bc649116d463c73b2474f7df4a3
habenz <haben.zaidt@gmail.com>
2025-01-22 21:20:11 -0500

    use the lookup table and select by name

--- src/App.js
+++ src/App.js
@@ -1,95 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
-import ColorPalette from './ColorPallete';
-import {useState} from 'react';
-import colors_json from './true-winter-colors-hex.json';
+import logo from "./logo.svg";
+import "./App.css";
+import ColorPalette from "./ColorPallete";
+import { useState } from "react";
+import colors_json from "./true-winter-colors-hex.json";
+import palettes from "./palettes.json";
 
 function App() {
-  const [selectedColorCodes, setSelectedColorCodes] = useState([]);
+  const [selectedColors, setSelectedColors] = useState([]);
 
   // Make a dictionary "Color Name" -> {TCX, Hex RGB value, Color Name}
   const colorLookUpTable = {};
   colors_json.colors.forEach((color_struct) => {
-    colorLookUpTable[color_struct.name] = color_struct
-  })
-  console.log(colorLookUpTable)
+    colorLookUpTable[color_struct.name] = color_struct;
+  });
 
-  const colors1 = {
-    complementaryColors: [
-      { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' },
-      { code: '17-3934', name: 'Persian Jewel', color: '#7B88C7' },
-      { code: '19-3952', name: 'Surf the Web', color: '#1B365D' }
-    ],
-    neutrals: [
-      { code: '19-3933', name: 'Medieval Blue', color: '#253046' },
-      { code: '11-4001', name: 'Brilliant White', color: '#F5F5FF' }
-    ],
-    accents: [
-      { code: '11-0620', name: 'Elfin Yellow', color: '#F3E5AB' },
-      { code: '13-6008', name: 'Misty Jade', color: '#B5D5C5' },
-      { code: '16-3617', name: 'Sheer Lilac', color: '#E0B0FF' },
-      { code: '19-3730', name: 'Gentian Violet', color: '#483248' }
-    ],
-  }
-  
-  const colors2 = {
-      complementaryColors: [
-      { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' },
-      { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' },
-      { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' }
-    ],
-    neutrals: [
-      { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' },
-      { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' }
-    ],
-    accents: [
-      { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' },
-      { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' },
-      { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' },
-      { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' }
-    ],
-  }
-
-  const colors = [colors1, colors2]
+  // const colors1 = {
+  //   complementaryColors: [
+  //     { code: '13-4403', name: 'Silver Birch', color: '#D3D3D3' },
+  //     { code: '17-3934', name: 'Persian Jewel', color: '#7B88C7' },
+  //     { code: '19-3952', name: 'Surf the Web', color: '#1B365D' }
+  //   ],
+  //   neutrals: [
+  //     { code: '19-3933', name: 'Medieval Blue', color: '#253046' },
+  //     { code: '11-4001', name: 'Brilliant White', color: '#F5F5FF' }
+  //   ],
+  //   accents: [
+  //     { code: '11-0620', name: 'Elfin Yellow', color: '#F3E5AB' },
+  //     { code: '13-6008', name: 'Misty Jade', color: '#B5D5C5' },
+  //     { code: '16-3617', name: 'Sheer Lilac', color: '#E0B0FF' },
+  //     { code: '19-3730', name: 'Gentian Violet', color: '#483248' }
+  //   ],
+  // }
 
   function includesSelectedColors(colorPallete) {
     // if no color is selected, always return true
-    if (selectedColorCodes.length == 0) {
+    if (selectedColors.length == 0) {
       return true;
     }
 
-    const colorsInPallete = [...colorPallete.complementaryColors, ...colorPallete.neutrals, ...colorPallete.accents];
-    return selectedColorCodes.every(colorCode => colorsInPallete.map((struct) => struct.code).includes(colorCode))
+    const colorsInPallete = [
+      ...colorPallete.complementary,
+      ...colorPallete.neutrals,
+      ...colorPallete.accents,
+    ];
+    return selectedColors.every((color) => colorsInPallete.includes(color));
   }
-  
+
   return (
     <>
       <div className="flex flex-wrap gap-4">
-        {
-          colors.filter((colorPallete) => includesSelectedColors(colorPallete)).map((colorPallete, i) => (
-            <ColorPalette 
+        {palettes
+          .filter((colorPallete) => includesSelectedColors(colorPallete))
+          .map((colorPallete, i) => (
+            <ColorPalette
               key={i}
               colors={colorPallete}
               onColorClick={onColorClick}
-              selectedColorCodes={selectedColorCodes}
-          />
-        ))}
+              selectedColors={selectedColors}
+              colorLookUpTable={colorLookUpTable}
+            />
+          ))}
       </div>
     </>
   );
 
-  function onColorClick(colorCode) {
-    const newSelectedColorCodes = selectedColorCodes.slice();
+  function onColorClick(colorName) {
+    const newSelectedColors = selectedColors.slice();
 
-    if (selectedColorCodes.includes(colorCode)) {
-      newSelectedColorCodes.splice(newSelectedColorCodes.indexOf(colorCode), 1);
+    if (selectedColors.includes(colorName)) {
+      newSelectedColors.splice(newSelectedColors.indexOf(colorName), 1);
     } else {
-      newSelectedColorCodes.push(colorCode);
+      newSelectedColors.push(colorName);
     }
 
-    console.log(newSelectedColorCodes);
-    setSelectedColorCodes(newSelectedColorCodes);
-    
+    console.log(newSelectedColors);
+    setSelectedColors(newSelectedColors);
+
     // Q: didn't update yet?
     // console.log(selectedColorCodes)
   }
--- src/ColorPallete.js
+++ src/ColorPallete.js
@@ -1,61 +1,70 @@
 import React from "react";
 
 const ColorPalette = ({
-  colors: { complementaryColors, neutrals, accents },
+  colors: { complementary, neutrals, accents },
   onColorClick,
-  selectedColorCodes,
+  selectedColors,
+  colorLookUpTable,
 }) => {
   return (
     <>
       <div className="grid grid-cols-[repeat(7,_1fr)_auto] grid-rows-4 gap-4 h-72 w-115 overflow-hidden">
         {/* React needs "key" to identify each div relative to parent */}
-        {complementaryColors.map((color, i) => {
-          const shouldHighlightColor = selectedColorCodes.includes(color.code);
-          return (
-            // {`${javascript whatever}`} used as f-string'
-            <div
-              key={color.name}
-              className={`relative box-border 
+        {complementary
+          .map((colorName) => colorLookUpTable[colorName])
+          .map((color, i) => {
+            const shouldHighlightColor = selectedColors.includes(color.name);
+            return (
+              // {`${javascript whatever}`} used as f-string'
+              <div
+                key={color.name}
+                className={`relative box-border 
                 border p-1 ${shouldHighlightColor ? "" : "border-transparent"} 
                 rounded bg-clip-content 
                 col-span-2 row-span-3 col-start-${i * 2 + 1}`}
-              style={{ backgroundColor: color.color }}
-              onClick={() => onColorClick(color.code)}
+                style={{ backgroundColor: color.hex }}
+                onClick={() => onColorClick(color.name)}
+              >
+                <div className="absolute bottom-0 m-2">
+                  <div>{color.name}</div>
+                  <div>{color.code} TCX</div>
+                </div>
+              </div>
+            );
+          })}
+        {neutrals
+          .map((colorName) => colorLookUpTable[colorName])
+          .map((color, i) => (
+            <div
+              key={color.name}
+              className={`relative col-span-3 col-start-${
+                i * 3 + 1
+              } row-start-4`}
+              style={{ backgroundColor: color.hex }}
+              onClick={() => onColorClick(color.name)}
             >
               <div className="absolute bottom-0 m-2">
                 <div>{color.name}</div>
                 <div>{color.code} TCX</div>
               </div>
             </div>
-          );
-        })}
-        {neutrals.map((color, i) => (
-          <div
-            key={color.name}
-            className={`relative col-span-3 col-start-${i * 3 + 1} row-start-4`}
-            style={{ backgroundColor: color.color }}
-            onClick={() => onColorClick(color.code)}
-          >
-            <div className="absolute bottom-0 m-2">
-              <div>{color.name}</div>
-              <div>{color.code} TCX</div>
-            </div>
-          </div>
-        ))}
-        {accents.map((color, i) => (
-          <>
-            <div
-              key={color.name}
-              className={`col-start-7 row-start-${i}`}
-              style={{ backgroundColor: color.color }}
-              onClick={() => onColorClick(color.code)}
-            ></div>
-            <div className={`col-start-8 row-start-${i}`}>
-              <div>{color.name}</div>
-              <div>{color.code} TCX</div>
-            </div>
-          </>
-        ))}
+          ))}
+        {accents
+          .map((colorName) => colorLookUpTable[colorName])
+          .map((color, i) => (
+            <>
+              <div
+                key={color.name}
+                className={`col-start-7 row-start-${i}`}
+                style={{ backgroundColor: color.hex }}
+                onClick={() => onColorClick(color.name)}
+              ></div>
+              <div className={`col-start-8 row-start-${i}`}>
+                <div>{color.name}</div>
+                <div>{color.code} TCX</div>
+              </div>
+            </>
+          ))}
       </div>
     </>
   );
